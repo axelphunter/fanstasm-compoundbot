1b627b1bcd7d5a73d513348703a73ff78336f43b
Axel Hunter <axelphunter@gmail.com>
2022-04-14 13:02:35 +0200

    Added more logs

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ const contract = new ethers.Contract(
   addresses.contract,
   [
     'function getReward() public',
-    'function stake(uint256 amount, bool lock) external'
+    'function stake(uint256 amount, bool lock) external',
+    'function totalBalance(address user) external view returns (uint256 amount)'
   ],
   account
 );
@@ -38,20 +39,44 @@ const fxm = new ethers.Contract(
   account
 )
 
+const expectedBlockTime = 3000; // Three seconds
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 app.listen(process.env.PORT || 4000, function () {
-  console.log('Server started');
-  cron.schedule('0 0 */1 * * *', async () => {
+  console.log("Let's gooo!");
+
+  const run = async () => {
     try {
       // Withdraw all available rewards.
       await contract.getReward()
+      await sleep(expectedBlockTime)
       // Query available FXM balance.
-      const balance = await fxm.balanceOf(addresses.recipient);
+      let balance = await fxm.balanceOf(addresses.recipient);
       // Stake available FXM balance.
       await contract.stake(balance, true);
-      console.log(`Staked ${balance.toString()}`)
+      await sleep(expectedBlockTime)
+      // Get total locked 
+      let totalLocked = await contract.totalBalance(addresses.recipient)
+      totalLocked = parseFloat(ethers.utils.formatEther(totalLocked)).toFixed(3)
+      balance = parseFloat(ethers.utils.formatEther(balance)).toFixed(3)
+
+      console.log(`
+      ==================\n
+      Reward claimed: ${balance} FXM\n
+      Total locked: ${totalLocked} FXM\n
+      ==================\n`)
     } catch (e) {
       console.log(e)
     }
-  });
+  };
+
+  run()
+
+  cron.schedule('0 0 */1 * * *', async () => {
+    run()
+  })
 });
 
